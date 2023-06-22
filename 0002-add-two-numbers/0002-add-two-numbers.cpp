60f45ac0c1350ee2fb09c6f3558c33da89e6bea1
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-23 01:26:15 +0530

    Time: 57 ms (5.60%), Space: 70.9 MB (94.35%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum=0;int carry=0; int data=0;
+        ListNode* prev;  ListNode* ans;
+        ans=l1;
+        
+        while(l1!=NULL && l2!=NULL)
+        {
+           sum=l1->val + l2->val +carry;
+           data=sum%10;
+            carry=sum/10;
+            l1->val=data;
+            prev=l1;
+            l1=l1->next;
+            l2=l2->next;   
+        }
+        if(l1==NULL && l2==NULL)
+        {
+          if(carry>0)
+         {
+             ListNode* add=new ListNode(carry) ;
+             add->next=NULL;
+             prev->next=add;
+         }   
+        }
+       else if(l1==NULL)
+        { prev->next=l2;
+         while(l2!=NULL)
+         {
+             sum=carry+l2->val;
+         data=sum%10;
+         carry=sum/10;
+             
+         l2->val=data;
+             prev=prev->next;
+         l2=l2->next;
+         }
+         if(carry>0)
+         {
+             ListNode* add=new ListNode(carry) ;
+             add->next=NULL;
+             prev->next=add;
+         }
+        }
+         else
+         {
+         while(l1!=NULL)
+         {
+             sum=carry+l1->val;
+         data=sum%10;
+         carry=sum/10;
+         l1->val=data;
+           prev=prev->next;   
+         l1=l1->next;
+         }
+          if(carry>0)
+         {
+             ListNode* add=new ListNode(carry) ;
+             add->next=NULL;
+             prev->next=add;
+         }   
+        }
+       return ans ;
+    }
+};
