07233837785b2c80df09322216bdb0436ec4b633
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-26 12:17:26 +0530

    Time: 43 ms (95.86%), Space: 14.5 MB (60.80%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,49 +9,34 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* node=new ListNode(-1); node->next=NULL;
-        int a=0;int b=0;
-        ListNode *cur1,*cur2; cur1=headA,cur2=headB;
-        while(cur1)
-        {
-
-          a++; cur1=cur1->next;
-        //headA=headA->next;
-            //headB=headB->next;
-        
-        }
-        while(cur2)
-        {
-
-          b++; cur2=cur2->next;
-       // headB=headB->next;
-            //headB=headB->next;
-        
-        }
-       // headA=cur1,headB=cur2;
-        if(a>b)
-        {
-            while(a>b)  {headA=headA->next; a--;}
-                
-        }
-        else if(b>a) 
-        {
-           while(b>a)    {headB=headB->next;
-               
-               b--;}
-        
-        }
-        while(headA && headB)
-        {
-
-        if(headA==headB)
-        { node->next=headA; break;
-
-        }
-headB=headB->next;
-        headA=headA->next; 
-        
-        }
-        return node->next;
+    //getting the tail
+    ListNode* tail = headA;
+    while(tail->next){
+        tail = tail->next;
     }
+
+    //creating a loop
+    tail->next = headA;
+
+    //detecting and finding the intersection
+    ListNode *fast = headB, *slow = headB;
+
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow==fast) {
+            slow = headB;
+            while(slow!=fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            //undoing the loop
+            tail->next = NULL;
+            return slow;
+        };
+    }
+    tail->next = NULL;
+    return NULL;
+}
 };
