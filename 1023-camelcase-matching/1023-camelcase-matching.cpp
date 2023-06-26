ffd48fe046b17c734ebe0b56ef10bfef6db383f7
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-26 15:29:12 +0530

    Time: 5 ms (38.00%), Space: 7.1 MB (86.86%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    vector<bool> camelMatch(vector<string>& queries, string pattern) {
-      vector<bool>query_ans(queries.size(),0) ; 
-        for(int i=0;i<queries.size();i++)
-        {    // string flow="";
-            int prev=-1;
-            bool flag= false; int p=0; int q=0;
-             for(int k=0;k<queries[i].size();k++)
-            {
-                if('A'<= queries[i][k] and queries[i][k]<='Z') q++;
-            }
-             for(int j=0;j<pattern.size();j++)
-        
-             {  
-                 if('A'<=pattern[j] and pattern[j]<='Z') p++;
-                 
-                 int ind;
-                 if(prev==-1)ind= queries[i].find(pattern[j]);
-                    else ind=queries[i].find(pattern[j],prev+1);
-                    
-             if( ind==-1 )
-             {
-                query_ans[i]=0; flag= true;break;
-             }
-             
-             else {prev=ind; }
-             
-             }
-           
-         if(!flag && q==p) query_ans[i]=1;
-        
+    
+    bool isSubSeq(string &s, string &t) {
+        int j = 0;
+        for(char c:s) {
+            if(c >= 'A' && c <= 'Z' && c != t[j]) return false;
+            if(c == t[j]) j++;
         }
-        return query_ans;
+        return j == t.size();
+    }
+    
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        vector<bool> ans;
         
+        for(auto &q: queries) {
+            ans.push_back(isSubSeq(q, pattern));
+        }
+        
+        return ans;
     }
 };
