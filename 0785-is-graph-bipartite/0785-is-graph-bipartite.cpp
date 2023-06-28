f7ab2040ab992dc0edd153fba5c3713599ec4381
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-29 00:27:51 +0530

    Time: 29 ms (46.20%), Space: 15.6 MB (7.90%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool bfs(int sv,vector<vector<int>> graph, vector<int>&color, vector<bool>&vis){
+    bool bfs(int sv,vector<vector<int>> graph, vector<int>&color){
     
     queue<int>q;
     q.push(sv);
-    vis[sv]=true; //marked as visited
+    //vis[sv]=true; //marked as visited
     color[sv]=1;  //markes color of sv as 1  
     //colors available are 1 and 0 and initially it is -1
     
@@ -16,7 +16,7 @@ public:
         for(int x:graph[curr]){
             
             if(color[x]==-1){  //if color == -1 i.e. not visisted 
-                vis[x]=true;
+                //vis[x]=true;
                 color[x]=!color[curr];  //complement of curr i.e. 0
                 q.push(x);
             }
@@ -32,12 +32,12 @@ public:
 
 bool isBipartite(vector<vector<int>>& graph) {
     
-    vector<bool>vis(graph.size(),false);  //visited array
+    //vector<bool>vis(graph.size(),false);  //visited array
     vector<int> color(graph.size(),-1);  //color array
     
     for(int i=0;i<graph.size();i++){  //tarversing all the nodes
-        if(!vis[i]){
-            bool ans=bfs(i,graph,color,vis);
+        if(color[i]==-1){
+            bool ans=bfs(i,graph,color);
             if(!ans){
                 return false;
             }
