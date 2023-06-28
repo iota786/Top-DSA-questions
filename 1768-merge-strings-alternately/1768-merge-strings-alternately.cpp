ed95bfd9d976eb16ef0b0c48f3ca912f24122189
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-29 02:01:42 +0530

    Time: 5 ms (12.61%), Space: 6.5 MB (18.53%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-       string ans="";
-        int mx=max(word1.size(),word2.size());
-        for(int i=0;i<mx;i++){
-            if(i<word1.size())ans+=word1[i];
-            if(i<word2.size())ans+=word2[i];
+      // string ans="";
+       // int mx=max(word1.size(),word2.size());
+        int k=0;
+        for(int i=0;i<word1.size();i+=2){
+            word1.insert(word1.begin()+i+1,word2[k++]);
+            if(k==word2.size()) break;
         }
-        return ans;
+        if(k<word2.size())
+            word1+=word2.substr(k);
+        return word1;
     }
 };
