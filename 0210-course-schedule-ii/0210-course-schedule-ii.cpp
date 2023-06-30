3ff748a62bbafe178ce48e440dd59a5603e8678e
iota786 <96467922+iota786@users.noreply.github.com>
2023-06-30 17:26:53 +0530

    Time: 17 ms (95.47%), Space: 13.5 MB (73.78%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+     vector<int> toposort(vector<vector<int>>&adj)
+    {
+
+    vector<int>indegrees(adj.size());
+        for(int i=0;i<adj.size();i++)
+        {
+            for(auto &node:adj[i])
+            {
+                indegrees[node]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<indegrees.size();i++)
+        {
+            if(indegrees[i]==0)
+                q.push(i);
+        }
+        vector<int>topo;
+        int c=0;
+        while(!q.empty())
+        {
+
+          int node=q.front();
+            q.pop();
+            topo.push_back(node);
+             //c++;
+            for(auto &i:adj[node]){
+               indegrees[i]--;
+                if(indegrees[i]==0)
+                    q.push(i);
+            
+            }
+        
+        
+        }
+         if(topo.size()==adj.size())
+        return topo;
+         else
+             return {};
+    
+    
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(auto &i:prerequisites)
+        {
+            adj[i[1]].push_back(i[0]);
+        }
+        return toposort(adj);
+        
+    }
+};
