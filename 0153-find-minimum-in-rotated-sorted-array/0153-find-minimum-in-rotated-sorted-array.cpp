1030586a99679bc4e637cedc58edc48ab3f60647
iota786 <96467922+iota786@users.noreply.github.com>
2023-07-01 23:50:10 +0530

    Time: 5 ms (49.56%), Space: 10.1 MB (50.62%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -3,15 +3,20 @@ public:
     int findMin(vector<int>& nums) {
         int l=0;
         int h=nums.size()-1;
+        int ans=0;
         while(l<h)
         {
             int mid=(l+h)/2;
             if(nums[mid]<=nums[h])
+            {
+                ans=nums[mid];
+            
             h=mid;
+            }
             else
                 l=mid+1;
         }
-        return nums[l];
+        return nums[h];
         
     }
 };
