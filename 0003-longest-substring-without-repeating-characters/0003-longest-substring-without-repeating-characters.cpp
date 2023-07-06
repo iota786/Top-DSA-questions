aa90d1b494bb11e2a0696590899c7e1101671f1c
iota786 <96467922+iota786@users.noreply.github.com>
2023-07-06 14:53:24 +0530

    Time: 8 ms (88.43%), Space: 8.2 MB (84.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,41 +1,21 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) {
-        long long i, j, res = 0;
-i = j = 0;
-
-unordered_map<char, int> Mp;
-
-while(j<str.length())
-
-{
-
-Mp[str[j]]++;
-
-if(Mp[str[j]] == 1)
-
-res= max(res, j-i+1); 
-
-else if(Mp[str[j]]> 1 )
-
-{ while(Mp[str[j]]> 1 )
-
-{
-
-Mp[str[i]]--; if(Mp[str[i]]==0) Mp.erase(str[i]);
-
-++i;
-
-
-
-}
-if(Mp[str[j]] ==1)
-
-res= max(res, j-i+1);
-}
-
-++j;
-
-} return res;  
+    int lengthOfLongestSubstring(string S) {
+        if(S.size()==0) return 0;
+         vector<int>dict(257, -1);
+        
+        int start = -1;
+        int ans = 0;
+        for(int j=0; j<S.size(); j++){
+            if(dict[(S[j])] > start){
+                start = dict[(S[j])];
+                
+            }
+            ans = max(ans, j-start);
+            
+            dict[(S[j])] = j;
+        }
+        
+        return ans;
     }
 };
