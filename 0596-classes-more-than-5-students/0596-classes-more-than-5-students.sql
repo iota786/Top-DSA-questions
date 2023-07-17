0ff2d1905a93d976d568c53a519bf6147aee7c9f
iota786 <96467922+iota786@users.noreply.github.com>
2023-07-17 12:00:53 +0530

    Time: 785 ms (15.46%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 #select class from Courses where (select class,count(student) from Courses group by class) >=5;
-select class from courses group by class having count(distinct student) >= 5;
+select class from courses group by class having count( class) >= 5;
