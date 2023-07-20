1d70e830466fc94b115db610206c84c9c2a56042
iota786 <96467922+iota786@users.noreply.github.com>
2023-07-20 12:42:13 +0530

    Time: 216 ms (54.72%), Space: 9.1 MB (56.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    int solveTab(int n)
+	{
+	    vector<int> dp (n + 1, INT_MAX) ;
+	    dp[0] = 0 ; 
+	    
+	    for (int i = 1 ; i <= n ; i ++)
+	    {
+	        for (int j = 1 ; j * j <= i ; j ++)
+	        {
+	            int temp = j * j ;
+	            if (i - temp >= 0)
+                {
+                    dp[i] = min (dp[i] , 1 + dp[i - temp]) ; 
+                }
+	        }
+	    }
+	    return dp[n] ; 
+	    
+	}
     int numSquares(int n) {
-        vector<int> memo(n+1, -1);
-        return solve(n, memo); 
-    }
-
-private:
-    int solve(int n, vector<int>& memo) {
-        if (n == 0) return 0;
-        if (memo[n] != -1) return memo[n];
-        int result = INT_MAX;
-        for (int i = 1; i*i <= n; i++) {
-            result = min(result, 1 + solve(n - i*i, memo));
-        }
-        return memo[n] = result;
+        return solveTab(n) ; 
     }
 };
