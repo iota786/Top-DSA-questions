28ff1847e92d23d0143fd6c15917e46afe6a244d
iota786 <96467922+iota786@users.noreply.github.com>
2023-07-29 01:50:23 +0530

    Time: 213 ms (99.96%), Space: 93.2 MB (99.96%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int> v(n,0),ans;
-        for(auto &i: edges){
-            v[i[1]]++;
+        // List to signify if the vertex has an incoming edge or not.
+        vector<bool> isIncomingEdgeExists(n, false);
+        for (vector<int>& edge : edges) {
+            isIncomingEdgeExists[edge[1]] = true;
         }
-        for(int i = 0; i < n; i++){
-            if(!v[i])ans.push_back(i);
+        
+        vector<int> requiredNodes;
+        for (int i = 0; i < n; i++) {
+            // Store all the nodes that don't have an incoming edge.
+            if (!isIncomingEdgeExists[i]) {
+                requiredNodes.push_back(i);
+            }
         }
-        return ans;
+        
+        return requiredNodes;
     }
 };
