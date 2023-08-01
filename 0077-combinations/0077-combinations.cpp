9b95d1ffa08d89a80f60294650326e6ad7424827
iota786 <96467922+iota786@users.noreply.github.com>
2023-08-02 00:04:04 +0530

    Time: 19 ms (87.17%), Space: 9 MB (83.81%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -4,7 +4,7 @@ public:
         vector<vector<int> >res;
         if(n<k)return res;
         vector<int> temp;
-        combine(res,temp,0,0,n,k);
+        combine(res,temp,1,0,n,k);
         //for(int i=0;i<result[0].size())
         return res;
     }
@@ -14,8 +14,8 @@ public:
             res.push_back(temp);
             return;
         }
-        for(int i = start;i<n;i++){
-            temp.push_back(i+1);
+        for(int i = start;i<=n;i++){
+            temp.push_back(i);
             combine(res,temp,i+1,num+1,n,k);
             temp.pop_back();
             }
